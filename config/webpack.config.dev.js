6aeb6c2362a4443e5382a5b984e28517f74cec6a
Absner Anaya <absrraniel@gmail.com>
2018-02-12 09:26:39 -0400

    configurando scss en webpack

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -189,9 +189,13 @@ module.exports = {
             ],
           },
           {
-            test: /\.sass$/,
+            test: /\.scss$/,
             include: paths.appSrc,
-            loaders: ["style", "css", "sass"]
+            loaders: [
+              require.resolve('style-loader'),
+              require.resolve('css-loader'),
+              require.resolve('sass-loader')
+            ]
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
