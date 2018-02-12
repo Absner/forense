fe6a7665e0b9be27ab29dde1274acb55a1f519f6
Absner Anaya <absrraniel@gmail.com>
2018-02-12 11:56:30 -0400

    creando mi primer componente

---
+++ src/components/header/header.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './header.component.scss';
+
+class HeaderComponent extends Component {
+    render() {
+        return (
+            <div className='color-prueba'>
+                Hola mundo
+            </div>
+        );
+    }
+}
+
+export default HeaderComponent;
