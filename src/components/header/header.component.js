39cf90a4d8fe1ec0e0df1027d6804b9e0d923667
Absner Anaya <absrraniel@gmail.com>
2018-02-12 14:23:14 -0400

    avances en el header

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import logo from './../../assets/logo.png';
 import './header.component.scss';
 
 class HeaderComponent extends Component {
     render() {
         return (
             <div className='color-prueba'>
-                Hola mundo
+                <div className='bgTope'>
+                    <div className='container absolute'>
+                        <div className='row'>
+                            <div className='col-md-12 pt-12 pb-12'>
+
+                                <div className='text-left' >
+                                    <ul className='navbar-nav mr-auto' >
+                                        <li>
+                                            <a href=''>
+                                                <img src={logo} alt="logo" />
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
