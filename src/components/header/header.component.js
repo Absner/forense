1d6c2066751c370c67c42e196b2153fe165b812b
Absner Anaya <absrraniel@gmail.com>
2018-02-15 11:11:09 -0400

    header 90% complete

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './../../assets/logo.png';
+import home_btn from './../../assets/home_btn.png';
+import click_btn from './../../assets/click_btn.png';
 import './header.component.scss';
 
 class HeaderComponent extends Component {
@@ -9,17 +11,35 @@ class HeaderComponent extends Component {
                 <div className='bgTope'>
                     <div className='container absolute'>
                         <div className='row'>
-                            <div className='col-md-12 pt-12 pb-12'>
+                            <div className='col-md-10 offset-md-1 pt-12 pb-12'>
+                                <nav className="navbar navbar-toggleable-md navbar-light">
+                                    <div className='text-left' >
+                                        <a href=''>
+                                            <img src={logo} alt="logo" />
+                                        </a>
+                                    </div>
 
-                                <div className='text-left' >
-                                    <ul className='navbar-nav mr-auto' >
-                                        <li>
-                                            <a href=''>
-                                                <img src={logo} alt="logo" />
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
+                                    <div className="right">
+                                        <ul className="navbar-nav">
+                                            <li className="nav-item active">
+                                                <a className="nav-link">
+                                                    <img src={home_btn} alt='home_btn' />
+                                                </a>
+                                            </li>
+                                            <li className="nav-item">
+                                                <a className="nav-link">
+                                                    <img src={click_btn} alt='click_btn' />
+                                                </a>
+                                            </li>
+                                            <li className="nav-item">
+                                                <input type='text' className='searchTextBox in-line'  name='search' />
+                                                <div className='search in-line'>
+                                                    <div className='searchLupa'></div>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </nav>
                             </div>
                         </div>
                     </div>
